d90e1fe2a5d2a11d1133c99c10cf5d04ab92d959
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-28 10:20:04 -0400

    not sure why not passing first test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for i in num_list:
+        if i%2 == 0:
+            evens.append(i)
+    # for i in num_list:
+    #     num_list.remove(i%2 != 0)
+    print(evens)
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = "!"
+    final_list = [i + exclamation for i in sentence_list]
+
+    # for i in sentence_list:
+    #     final_list.append(i + exclamation)
+    print(final_list)
+
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
