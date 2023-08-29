58f1ca3d4438d152c2992e59f008bf259c0b2509
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-28 23:32:57 -0400

    figured out test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    evens = []
-    for i in num_list:
-        if i%2 == 0:
-            evens.append(i)
+    return [i for i in num_list if i%2 == 0]
     # for i in num_list:
-    #     num_list.remove(i%2 != 0)
-    print(evens)
+    #     if i%2 == 0:
+    #         evens.append(i)
+    # print(evens)
 
-return_evens([0, 1, 3, 5, 7, 8, 9])
+# return_evens([1, 10, 2])
 
 def make_exclamation(sentence_list):
     exclamation = "!"
-    final_list = [i + exclamation for i in sentence_list]
+    return [i + exclamation for i in sentence_list]
 
     # for i in sentence_list:
     #     final_list.append(i + exclamation)
-    print(final_list)
+    # print(final_list)
 
 
-make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+# make_exclamation(["Hello", "I'm doing great", "Python is fun"])
